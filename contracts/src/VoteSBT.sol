c5b711fc4dcacc16befa81947695bfc122cbc34a
ququzone <xueping.yang@gmail.com>
2023-03-26 21:45:23 +0800

    add basic contract

--- contracts/src/VoteSBT.sol
+++ contracts/src/VoteSBT.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract VoteSBT {
-    uint256 public number;
+import "solmate/tokens/ERC721.sol";
+import "solmate/auth/Owned.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract VoteSBT is Owned, ERC721 {
+    string private baseURI;
+    uint256 public currentTokenId;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI
+    ) Owned(msg.sender) ERC721(_name, _symbol) {
+        baseURI = _baseURI;
     }
 
-    function increment() public {
-        number++;
+    function mint(address recipient) public payable onlyOwner returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+
+    function tokenURI(uint256 /*id*/) public view virtual override returns (string memory) {
+        return baseURI;
     }
 }
