f27cd33f4738897542209af4d86d2439160fd3f4
ququzone <xueping.yang@gmail.com>
2023-03-27 09:32:59 +0800

    deploy contracts

---
+++ contracts/README.md
@@ -0,0 +1,8 @@
+## Contracts
+
+### Deploy
+
+```
+forge script script/Deploy.s.sol:Deploy --broadcast -vvvv --legacy \
+  --rpc-url $ETH_RPC_URL --private-key $PRIVATE_KEY
+```
---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/VoteSBT.sol";
+
+contract Deploy is Script {
+    function run() external {
+        vm.startBroadcast();
+        new VoteSBT("I Vote", "IVOTE", "");
+        vm.stopBroadcast();
+    }
+}
