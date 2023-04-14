08b9d1902bcec4b29ea470a74aac14e8496cc5aa
ququzone <xueping.yang@gmail.com>
2023-04-14 10:44:01 +0800

    sent IIP-14 SBT

--- publisher/main.go
+++ publisher/main.go
@@ -12,12 +12,12 @@ func main() {
 	token := os.Getenv("TOKEN")
 	var skip uint64 = 0
 	for {
-		voters, err := fetcher.Fetch("0x2f97ac5cb8467fe16d4dd0b52d95aeba67e1196df2858baa710b238768a70b78", skip)
+		voters, err := fetcher.Fetch("0x38e56cff9f04b88c29447a6b71734274f65931cc8af049f82a268abb7474af0c", skip)
 		if err != nil {
 			log.Fatalf("fetch voter error: %v\n", err)
 		}
 		for _, v := range voters {
-			err := pusher.Push(token, v[2:], "1")
+			err := pusher.Push(token, v[2:], "2")
 			if err != nil {
 				log.Printf("post mint data to node error: %v\n", err)
 			}
